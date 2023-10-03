d9135b23050b383244fead1c95b6f29b740f6e74
shirinzarqaa <shirinzarqaa354@gmail.com>
2023-10-03 22:41:26 +0700

    assignment 5

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml , show_json,show_xml_by_id, show_json_by_id 
 from main.views import register, login_user , logout_user, increment_product, decrement_product, remove_product
-
+from main.views import edit_product
 
 
 app_name = "main"
@@ -19,5 +19,6 @@ urlpatterns = [
     path('add_product/<int:product_id>/', increment_product, name='add_product'),
     path('decrement_product/<int:product_id>/', decrement_product, name='decrement_product'),
     path('remove_product/<int:product_id>/',remove_product, name='remove_product'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
     
 ]
